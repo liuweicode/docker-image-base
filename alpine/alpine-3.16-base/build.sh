eb02fa794158625e0b1592d5032a6f4f9c42ed0b
liuwei <liuwei@top-list.com.cn>
2022-09-23 18:33:31 +0800

    修改构建脚本

--- alpine/alpine-3.16-base/build.sh
+++ alpine/alpine-3.16-base/build.sh
@@ -2,14 +2,18 @@
 
 set -eux
 
-docker build --no-cache -t tl-os:alpine-3.16-base -f Dockerfile .
+BUILD_NAME='tl-alpine'
+BUILD_VERSION='alpine-3.16-base'
 
-docker images | grep 'alpine-3.16-base'
+docker build --no-cache -t $BUILD_NAME:$BUILD_VERSION -f Dockerfile .
 
-IMAGETAG=$(docker images | grep 'alpine-3.16-base' | head -n 1 | awk '{print $3}')
+docker images | grep $BUILD_VERSION
+
+IMAGETAG=$(docker images | grep $BUILD_VERSION | head -n 1 | awk '{print $3}')
 
 echo $IMAGETAG
 
-docker tag $IMAGETAG toplist-registry.cn-shanghai.cr.aliyuncs.com/88/tl-os:alpine-3.16-base
+docker tag $IMAGETAG toplist-registry.cn-shanghai.cr.aliyuncs.com/88/$BUILD_NAME:$BUILD_VERSION
 
-docker push toplist-registry.cn-shanghai.cr.aliyuncs.com/88/tl-os:alpine-3.16-base
+docker push toplist-registry.cn-shanghai.cr.aliyuncs.com/88/$BUILD_NAME:$BUILD_VERSION
+
--- alpine/alpine-3.16-glibc-2.35-r0/build.sh
+++ alpine/alpine-3.16-glibc-2.35-r0/build.sh
@@ -2,14 +2,17 @@
 
 set -eux
 
-docker build --no-cache -t tl-os:alpine-3.16-glibc-2.35-r0 -f Dockerfile .
+BUILD_NAME='tl-alpine'
+BUILD_VERSION='alpine-3.16-glibc-2.35-r0'
 
-docker images | grep 'alpine-3.16-glibc-2.35-r0'
+docker build --no-cache -t $BUILD_NAME:$BUILD_VERSION -f Dockerfile .
 
-IMAGETAG=$(docker images | grep 'alpine-3.16-glibc-2.35-r0' | head -n 1 | awk '{print $3}')
+docker images | grep $BUILD_VERSION
+
+IMAGETAG=$(docker images | grep $BUILD_VERSION | head -n 1 | awk '{print $3}')
 
 echo $IMAGETAG
 
-docker tag $IMAGETAG toplist-registry.cn-shanghai.cr.aliyuncs.com/88/tl-os:alpine-3.16-glibc-2.35-r0
+docker tag $IMAGETAG toplist-registry.cn-shanghai.cr.aliyuncs.com/88/$BUILD_NAME:$BUILD_VERSION
 
-docker push toplist-registry.cn-shanghai.cr.aliyuncs.com/88/tl-os:alpine-3.16-glibc-2.35-r0
+docker push toplist-registry.cn-shanghai.cr.aliyuncs.com/88/$BUILD_NAME:$BUILD_VERSION
