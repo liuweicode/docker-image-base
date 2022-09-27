ccda476a40199a6b241a5b5b7cbb4feaeb620ab5
liuwei <liuwei@top-list.com.cn>
2022-09-27 22:24:51 +0800

    php 7.3.33 基础镜像

---
+++ php/7.3.33/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+
+BUILD_NAME='php'
+BUILD_VERSION='php-7.3.33-base'
+
+docker build --no-cache -t $BUILD_NAME:$BUILD_VERSION -f Dockerfile .
+
+docker images | grep $BUILD_VERSION
+
+IMAGETAG=$(docker images | grep $BUILD_VERSION | head -n 1 | awk '{print $3}')
+
+echo $IMAGETAG
+
+docker tag $IMAGETAG suxz/$BUILD_NAME:$BUILD_VERSION
+
+docker push suxz/$BUILD_NAME:$BUILD_VERSION
